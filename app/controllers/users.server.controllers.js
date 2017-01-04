b9af08431020283d5e92259bf99058ef3a338fcb
dayerygonzalez <dayery.gonzalez@gmail.com>
2017-01-04 11:49:09 -0500

    Funcion Eliminar
    Se creó la funcion de eliminar

--- app/controllers/users.server.controllers.js
+++ app/controllers/users.server.controllers.js
@@ -5,9 +5,15 @@
 
 var comentarioModelo = require('mongoose').model('comentarios');
 exports.obtenerInf = function(req,res,next){
-
     var comentario = new comentarioModelo (req.body)
-
-
 }
 
+exports.eliminarInf = function(req,res,next,id){
+
+req.comentarioModelo.remove(function (err) {
+    if (err) {
+      console.log(err)
+      return next(err)
+   			 }
+	});
+}
--- app/models/user.server.models.js
+++ app/models/user.server.models.js
@@ -6,13 +6,12 @@ var mongoose = require ('mongoose'),
     Schema = mongoose.Schema;
 
 /*Definir un nuevo UserSchema*/
-var imagenSchema = new Schema ({
-  imagen: {
-    type:String,
-    required: 'Url de la imgane obligatoria'
-  },
-  comentario: String
-}/*,{collection : 'imagenes'}*/); ///cambiar por cualquier base de datos
+var comentariosSchema = new Schema ({
+  
+	comentario:{type:String,required:'Comentario requerido'},
+	usuario:{type:String,required:'Usuario requerido'}
+
+  }); ///cambiar por cualquier base de datos
 
 /*crear el modelo 'user' a partir de 'UserSchema'*/
-mongoose.model('imagenes', imagenSchema);
+mongoose.model('comentarios', comentariosSchema);
