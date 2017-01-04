5f8028b0c1107048990b25ddc3418132d3c3cf6d
cardozob <bryan1395osorio@gmail.com>
2017-01-04 18:25:41 -0500

    agregar router

--- app/controllers/users.server.controllers.js
+++ app/controllers/users.server.controllers.js
@@ -4,6 +4,8 @@
 /*Cargar el model Mongoose 'User'*/
 
 var comentarioModelo = require('mongoose').model('comentarios');
+
+
 exports.obtenerInf = function(req,res,next){
     var comentario = new comentarioModelo (req.body)
 }
